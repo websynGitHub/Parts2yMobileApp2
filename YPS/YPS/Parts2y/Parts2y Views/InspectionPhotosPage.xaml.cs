dfb1e37c4396be42569c80e63e8e6ab205d03309
chandrasekhar.vazza <chandrasekhar.vazza@e4374817-edc6-ba4a-95a8-b6457f00fac4>
2021-03-15 09:41:39 +0000

    inspection photos uploading

---
+++ YPS/YPS/Parts2y/Parts2y Views/InspectionPhotosPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using YPS.Model;
+using YPS.Parts2y.Parts2y_View_Models;
+
+namespace YPS.Parts2y.Parts2y_Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InspectionPhotosPage : ContentPage
+    {
+        InspectionPhotoUploadViewModel Vm;
+        public InspectionPhotosPage(int tagId, InspectionConfiguration inspectionConfiguration)
+        {
+            InitializeComponent();
+            BindingContext = Vm = new InspectionPhotoUploadViewModel(Navigation, this, tagId, inspectionConfiguration);
+        }
+
+        private async void Back_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
