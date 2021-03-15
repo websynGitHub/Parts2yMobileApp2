6e3bbad973f457a5c35579bba4476af81b743265
chandrasekhar.vazza <chandrasekhar.vazza@e4374817-edc6-ba4a-95a8-b6457f00fac4>
2021-03-15 17:15:54 +0000

    added qid for get inspectoion photos

--- YPS/YPS/Service/YPSService.cs
+++ YPS/YPS/Service/YPSService.cs
@@ -864,10 +864,10 @@ namespace YPS.Service
         /// </summary>
         /// <param name="tagId"></param>
         /// <returns></returns>
-        public async Task<InspectionPhotosResponse> GeInspectionPhotosService(int tagId)
+        public async Task<InspectionPhotosResponse> GeInspectionPhotosService(int tagId, int questionId)
         {
             RestClient restClient = new RestClient();
-            return await restClient.GeInspectionPhotosClient(tagId);
+            return await restClient.GeInspectionPhotosClient(tagId, questionId);
         }
 
         /// <summary>
