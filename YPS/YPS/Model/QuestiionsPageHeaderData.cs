3817b62dc5db8a983b674135fef255d44b598f1d
chandrasekhar.vazza <chandrasekhar.vazza@e4374817-edc6-ba4a-95a8-b6457f00fac4>
2021-03-15 14:20:47 +0000

    questiojnspage header

---
+++ YPS/YPS/Model/QuestiionsPageHeaderData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPS.Model
+{
+    public class QuestiionsPageHeaderData
+    {
+        public string VINLabel { get; set; }
+        public string VINLabelValue { get; set; }
+        public string IdentCodeLabel { get; set; }
+        public string IdentCodeLabelValue { get; set; }
+        public string ConditionNameLabel { get; set; }
+        public string ConditionNameLabelValue { get; set; }
+
+    }
+}
