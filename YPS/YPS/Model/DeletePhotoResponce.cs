d57fe5377864e81fb8c597f5f835e6634260dbf4
chandrasekhar.vazza <chandrasekhar.vazza@e4374817-edc6-ba4a-95a8-b6457f00fac4>
2021-03-19 06:34:33 +0000

    delete inspection photos

---
+++ YPS/YPS/Model/DeletePhotoResponce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPS.Model
+{
+    public class DeletePhotoResponce
+    {
+        public string message { get; set; }
+        public int status { get; set; }
+        public string data { get; set; }
+    }
+}
